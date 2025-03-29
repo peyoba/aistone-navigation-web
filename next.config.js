bbec0f598e4a4382f923537678b176efc0fad487
peyoba <396217818@qq.com>
2025-03-29 09:17:05 +0800

    修复: 添加Cloudflare Pages路由配置和Next.js静态导出设置

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig; 
