f57e64690675d5bed06b665199577902713b6923
peyoba <396217818@qq.com>
2025-03-29 09:33:38 +0800

    修复: 添加静态HTML页面和更多Cloudflare部署配置

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   distDir: 'out',
-  basePath: '',
-  assetPrefix: '',
 }
 
 module.exports = nextConfig; 
